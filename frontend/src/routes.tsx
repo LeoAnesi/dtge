239b942bffa82741e91c5846d68ec1b6e82aa3d7
leoa <leoa@theodo.fr>
2021-08-28 10:37:31 +0200

    feat: protect home where we'll display members

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,5 +1,7 @@
 import React, { lazy, Suspense } from 'react';
-import { Route, Switch } from 'react-router';
+import { useSelector } from 'react-redux';
+import { Redirect, Route, RouteProps, Switch } from 'react-router';
+import { getUserToken } from 'redux/Login';
 import Loader from './components/Loader/Loader';
 
 const Home = lazy(() => import('./pages/Home'));
@@ -10,10 +12,22 @@ export const PATHS = {
   LOGIN: '/login',
 };
 
+const ProtectedRoute = ({ ...routeProps }: RouteProps) => {
+  const userToken = useSelector(getUserToken);
+  const isAuthenticated = userToken !== null;
+  const authenticationPath = PATHS.LOGIN;
+
+  if (isAuthenticated) {
+    return <Route {...routeProps} />;
+  } else {
+    return <Redirect to={{ pathname: authenticationPath }} />;
+  }
+};
+
 const routes = (): JSX.Element => (
   <Suspense fallback={<Loader />}>
     <Switch>
-      <Route exact path={PATHS.HOME} component={Home} />
+      <ProtectedRoute exact path={PATHS.HOME} component={Home} />
       <Route path={PATHS.LOGIN} component={Login} />
     </Switch>
   </Suspense>
